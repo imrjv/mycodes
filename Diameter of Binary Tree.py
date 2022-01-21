aee065c11ee8c64cc26ccd3655f67dacf1a85240
rjvra <75006113+imrjv@users.noreply.github.com>
2022-01-21 10:04:38 -0500

    Update Diameter of Binary Tree.py

--- Diameter of Binary Tree.py
+++ Diameter of Binary Tree.py
@@ -5,17 +5,16 @@ class Node:
         self.right = None
 
 
-def diameter(root):
-    max_height = 0
+def diameter(root, maxi):
     if root is None:
         return 0
-    else:
 
-        left_height = diameter(root.left)
-        right_height = diameter(root.right)
+    lh = diameter(root.left, maxi)
+    rh = diameter(root.right, maxi)
 
-    max_height = max(max_height, left_height+right_height)
-    return max(left_height, right_height) + 1
+    maxi[0] = max(maxi[0], lh+rh+1)
+
+    return 1+max(lh, rh)
 
 
 root = Node(1)
@@ -23,14 +22,18 @@ root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5)
-root.left.left.left = Node(7)
-root.left.left.right = Node(9)
-root.left.left.left.left = Node(10)
+root.right.left = Node(6)
+root.right.right = Node(7)
+root.left.left.left = Node(8)
+root.right.right.right = Node(10)
 
-if diameter(root) > 0:
-    print("The Diameter of a binary tree is", diameter(root)+1)
-else:
-    print(0)
-    
-    
-# Ans => The Diameter of a binary tree is 6.
+maxi = [0]
+diameter(root, maxi)
+print(maxi[0])
+
+'''
+
+Ans => 7
+Time Complexity = O(n)
+
+'''
